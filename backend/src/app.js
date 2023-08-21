bc2d4f53a77ec50bef773a877c96f84327135cf5
Carlos Eduardo da Silva <carlao0010@hotmail.com>
2023-08-21 16:02:19 -0300

    Req 04 OK

--- backend/src/app.js
+++ backend/src/app.js
@@ -24,6 +24,13 @@ app.get('/products/:productId', async (req, res) => {
 app.post('/products', async (req, res) => {
   const { name } = req.body;
 
+  if (!name) return res.status(400).json({ message: '"name" is required' });
+  if (name.length < 5) {
+    return res.status(422).json({ 
+      message: '"name" length must be at least 5 characters long',
+    });
+  }
+
   const productId = await productsModel.insert(name);
 
   return res.status(201).json(productId);
